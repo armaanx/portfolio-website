6975a92d711956d024f27e58eb10fbdb813f4dfa
armaanx <103936457+armaanx@users.noreply.github.com>
2023-03-26 23:17:11 +0530

    font change

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
     setShow(!show);
   }
   return (
-    <div className="snap-y h-screen snap-mandatory bg-[#121516] overflow-auto text-white z-1 scroll-smooth scrollbar-hide">
+    <div className="font-[Montserrat] snap-y h-screen snap-mandatory bg-[#121516] overflow-auto text-white z-1 scroll-smooth scrollbar-hide">
       <Head>
         <title>{"Armaan's Portfolio"}</title>
       </Head>
